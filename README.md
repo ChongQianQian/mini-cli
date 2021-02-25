0d0e6147c77242315ae8436ee28c5c99ca4cfaea
chongqianqian <yunle@dtstack.com>
2021-02-25 16:37:10 +0800

    feat:add inruire和commander 采集和处理用户数据

--- README.md
+++ README.md
@@ -34,13 +34,13 @@ mini init <template-name> <project-name> 基于指定的模板进行项目创建
 mini list 列出所有可用模板
 ```
 
-#### 使用 commander 解析命令行工具
-
+#### 1.commander包解析命令行工具
+> 完整的 node.js 命令行解决方案。
 [官方 npm 链接](https://github.com/tj/commander.js/blob/master/Readme_zh-CN.md#%e4%be%8b%e5%ad%90)
 
-#### 模板代码下载方式
-
+#### 2.download-git-repo下载代码
 > 安装 download-git-repo 可以下载指定 git 地址的代码
+[官方 npm 地址](https://www.npmjs.com/package/download-git-repo)
 
 ```bash
 npm install download-git-repo
@@ -57,4 +57,39 @@ download(
   }
 );
 //这样在控制台中输入 mini init tpl-a proname 会在当前路径下创建文件名为filename的文件夹
+```
+
+#### 3.使用inquirer来交互输入
+```bash
+npm install inquirer
+```
+```js
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+```
+
+#### 4.handlebars处理用户输入
+> [官方地址](https://handlebarsjs.com/zh/installation/#npm-%E6%88%96-yarn-%EF%BC%88%E6%8E%A8%E8%8D%90%EF%BC%89)
+```bash
+npm install handlebars
+```
+```js
+const handlebars = require("handlebars");
+
+const packageNewContent = handlebars.compile(packageContent)(answers);
+// packageContent中对应的向导关键字 需要用{{name}}
+// 然后在控制台中输入：min tpl-a proname
 ```
--- index.js
+++ index.js
@@ -9,21 +9,24 @@ console.log("参考vuecli-搭建自己的脚手架工具");
 const { Command } = require("commander");
 const program = new Command();
 const download = require("download-git-repo");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const handlebars = require("handlebars");
 
 const templetes = {
   "tpl-a": {
     url: "https://github.com/ChongQianQian/tpl-a.git",
-    downurl:"github:ChongQianQian/tpl-a#main",
+    downurl: "github:ChongQianQian/tpl-a#main",
     describe: "初始化a模板",
   },
   "tpl-b": {
     url: "https://github.com/ChongQianQian/tpl-b.git",
-    downurl:"github:ChongQianQian/tpl-b#main",
+    downurl: "github:ChongQianQian/tpl-b#main",
     describe: "初始化b模板",
   },
   "tpl-c": {
     url: "https://github.com/ChongQianQian/tpl-c.git",
-    downurl:"github:ChongQianQian/tpl-c#main",
+    downurl: "github:ChongQianQian/tpl-c#main",
     describe: "初始化c模板",
   },
 };
@@ -35,13 +38,76 @@ program
   .description("初始化项目模板")
   .action((templateName, projectName) => {
     //根据templateName下载对应的模板到本地并起名为 projectName
-    // console.log(templetes[templateName]); //env：输入的模板名称
+
+    //优化：判断项目名是否存在 如果存在的话 把之前的删除 否则新建
+    fs.exists(`./${projectName}`, (exists) => {
+      if (exists) {
+        let path = `./${projectName}`
+        deleteall(path)
+      }
+    });
+
+    //下载对应模板
     download(
       `${templetes[templateName].downurl}`,
       `${projectName}`,
       { clone: true },
       function (err) {
-        console.log(err ? "下载失败" : "下载成功");
+        if (err) {
+          return "下载失败";
+        } else {
+          //使用inquirer对用户的输入进行向导
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "请输入项目名:",
+                name: "name",
+                default: `${projectName}`, // 默认值
+              },
+              {
+                type: "input",
+                message: "请输入项目描述:",
+                name: "description",
+                default: "", // 默认值
+              },
+              {
+                type: "input",
+                message: "请输入项目作者:",
+                name: "author",
+                default: "", // 默认值
+              },
+            ])
+            .then((answers) => {
+              //{ name: 'proname4', description: '描述信息', author: '' }
+              //1.获取用户控制台输入结果
+              //2.读取package.json文件
+              //3.动态修改package.json文件对应的内容
+              //4.重新写入到package.json文件中
+
+              const packageContent = fs.readFileSync(
+                `${projectName}/package.json`,
+                "utf8"
+              );
+              const packageNewContent = handlebars.compile(packageContent)(
+                answers
+              );
+              console.log("packageNewContent: ", packageNewContent);
+
+              fs.writeFileSync(
+                `${projectName}/package.json`,
+                packageNewContent
+              );
+              console.log(`初始化模板${templateName}成功`);
+            })
+            .catch((error) => {
+              if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+              } else {
+                // Something else went wrong
+              }
+            });
+        }
       }
     );
   });
@@ -57,5 +123,24 @@ program
         `);
     }
   });
+
+//删除文件夹及文件
+function deleteall(path) {
+  var files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach(function (file, index) {
+      var curPath = path + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        // recurse
+        deleteall(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
 
 program.parse(process.argv);
